a95124e99d36a06c8b02aad18f40b51e5aeedff0
mic050r <s2216@e-mirim.hs.kr>
2024-10-02 10:44:13 +0900

    FEAT(#1) :: Before Advice 등록
    - 메소드 호출 전에 동작

--- AOP/src/main/java/org/example/advice/AspectClass.java
+++ AOP/src/main/java/org/example/advice/AspectClass.java
@@ -5,4 +5,8 @@ import org.aspectj.lang.ProceedingJoinPoint;
 // Aspect : 여러 개의 Advice를 모아 놓은 것 (Pointcut + Advice)
 public class AspectClass {
 
+    // Before Advice: 메서드 호출 전에 실행되는 로직
+    public void before() {
+        System.out.println("Before Method");
+    }
 }
