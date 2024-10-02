9f1bf46779db79715562f51f4e6fb27f35be24c5
mic050r <s2216@e-mirim.hs.kr>
2024-10-02 10:46:05 +0900

    FEAT(#1) :: Around Advice 등록
    - 메소드 호출 전과 후에 동작

--- AOP/src/main/java/org/example/advice/AspectClass.java
+++ AOP/src/main/java/org/example/advice/AspectClass.java
@@ -14,4 +14,19 @@ public class AspectClass {
     public void after() {
         System.out.println("After Method");
     }
+
+    // Around Advice: 메서드 호출 전후에 모두 실행되는 로직
+    public void around(ProceedingJoinPoint pjp) throws Throwable {
+        // 메서드 실행 전에 실행되는 로직
+        System.out.println("전에 around 호출 1");
+
+        // 실제 메서드를 실행
+        Object returnValue = pjp.proceed();
+
+        // 메서드 실행 후에 실행되는 로직
+        System.out.println("후에 around 호출 2");
+
+        // 반환 값을 로그에 남기는 등 추가 작업 가능.
+        System.out.println("Return value from around: " + returnValue);
+    }
 }
