fb2531ae7fc4b9075dc27ea9edf0aa4bc703a19f
mic050r <s2216@e-mirim.hs.kr>
2024-10-04 20:23:10 +0900

    FEAT(#6) :: GET @RequestParam 실습
    - required = false : 값이 없을 때 자동으로 null을 넣어줌
    - defaultValue="22" : 초기값을 지정해줌

--- mvc/src/main/java/org/example/mvc/get/GetController.java
+++ mvc/src/main/java/org/example/mvc/get/GetController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 
 @Controller
@@ -12,7 +13,7 @@ public class GetController {
     @GetMapping("/test1")
     public String test1(HttpServletRequest http) {
         String d1 = http.getParameter("data1");
-        String d2= http.getParameter("data2");
+        String d2 = http.getParameter("data2");
         String d3 = http.getParameter("data3");
 
         System.out.println("data1 : " + d1);
@@ -26,7 +27,7 @@ public class GetController {
     public String test2(HttpServletRequest http) {
         String d1 = http.getParameter("data1"); // 단일 값 가져오기
         String d3[] = http.getParameterValues("data3"); // 배열 값 가져오기
-        for(String s : d3) System.out.println(s);
+        for (String s : d3) System.out.println(s);
 
         return "result";
     }
@@ -35,7 +36,7 @@ public class GetController {
     public String test3(WebRequest http) {
         String d1 = http.getParameter("data1"); // 단일 값 가져오기
         String d3[] = http.getParameterValues("data3"); // 배열 값 가져오기
-        for(String s : d3) System.out.println(s);
+        for (String s : d3) System.out.println(s);
         return "result";
     }
 
@@ -44,8 +45,22 @@ public class GetController {
                        @PathVariable("data2") int data2,
                        @PathVariable("data3") int data3) {
         System.out.println("data1 : " + data1);
-        int sum = data1+data2+data3;
+        int sum = data1 + data2 + data3;
         System.out.println("int :" + sum);
+        return "result";
+    }
+
+    @GetMapping("test5")
+    public String test(@RequestParam("data1") String data1,
+                       @RequestParam("data2") String data2,
+                       @RequestParam("data3") String[] data3,
+                       @RequestParam(value = "data4", required = false, defaultValue = "initial") String data4) {
+        // required = false : 값이 없을 때 자동으로 null을 넣어줌
+        // defaultValue="22" : 초기값을 지정해줌
+        for (String s : data3) {
+            System.out.println("data3 : " + s);
+        }
+        System.out.println("data4 : " + data4);
         return "result";
     }
 }
