8cab840eab728d4a2804f9d7ac5889bcd0d61e3f
mic050r <s2216@e-mirim.hs.kr>
2024-10-04 19:11:40 +0900

    FEAT(#2) :: @RequestMapping 으로 공통된 엔드포인트 작성하기

--- mvc/src/main/java/org/example/mvc/home/TestController.java
+++ mvc/src/main/java/org/example/mvc/home/TestController.java
@@ -2,22 +2,24 @@ package org.example.mvc.home;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/sub")
 public class TestController {
-    @GetMapping("/sub/test1")
+    @GetMapping("test1")
     @ResponseBody // return값이 텍스트로 그냥 출력
     public String test() {
         return "test";
     }
 
-    @GetMapping("/sub/test2")
+    @GetMapping("test2")
     public String test2() {
         return "test2";
     }
 
-    @GetMapping("/sub/test3")
+    @GetMapping("test3")
     public String test3() {
         return "test3";
     }
