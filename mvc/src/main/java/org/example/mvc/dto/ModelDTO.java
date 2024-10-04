235e718690cdb8402e13787c216c6c8e03044056
mic050r <s2216@e-mirim.hs.kr>
2024-10-04 22:20:51 +0900

    FEAT(#8) :: Model form action controller 연습
    - @PathVariable, @RequestParam, @ModelAttribute를 이용해서 입력받은 값을 DTO에 저장하고 Model을 이용해서 View로 보내기
    - 입력한 값있는지 : th:if="${result}"
    - url 연결 : th:href="@{/}"
    - 값에 문자 붙이기 : th:text="{$'문자 + result}"

---
+++ mvc/src/main/java/org/example/mvc/dto/ModelDTO.java
@@ -0,0 +1,9 @@
+package org.example.mvc.dto;
+
+import lombok.Data;
+
+@Data
+public class ModelDTO {
+    int num;
+    String str;
+}
--- mvc/src/main/java/org/example/mvc/home/ModelController.java
+++ mvc/src/main/java/org/example/mvc/home/ModelController.java
@@ -1,10 +1,12 @@
 package org.example.mvc.home;
 
+import org.example.mvc.dto.ModelDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/model")
 public class ModelController {
 
     @GetMapping("/test7")
@@ -12,5 +14,33 @@ public class ModelController {
         model.addAttribute("name", "kim");
         model.addAttribute("age", "30");
         return "test7";
+    }
+
+    @GetMapping("/test1")
+    public String test1() {
+        return "test10";
+    }
+
+    @GetMapping("/test2")
+    public String test2() {
+        return "test11";
+    }
+
+    @PostMapping("/test1")
+    public String sum(@RequestParam("num") int num, Model model) {
+        model.addAttribute("result", num);
+        return "test10";
+    }
+
+    @PostMapping("/test/{num}")
+    public String sum2(@PathVariable("num") int num, Model model) {
+        model.addAttribute("result", num);
+        return "test10";
+    }
+
+    @PostMapping("/test2")
+    public String sum3(@ModelAttribute ModelDTO modelDTO, Model model) {
+        model.addAttribute("result", modelDTO);
+        return "test11";
     }
 }
