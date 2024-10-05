1b9cfd8f79aff6df9428dd793dc780ef98cb8ef3
mic050r <s2216@e-mirim.hs.kr>
2024-10-05 15:58:42 +0900

    FEAT :: DB Spring boot init commit
    - 라이브러리 : Spring Web, Lombok, Thymeleaf, Spring Boot Dev Tools, Oracle Driver, Mybatis Framework
    - 포트 번호 8000 변경

---
+++ DB/src/test/java/org/example/db/DbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
